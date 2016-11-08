bd8cd487ae859f6ebd7a070c5ec3eee0b90886fc
Marco <Marco@MarcoTab.edu.ds.fhnw.ch>
2016-11-08 16:34:50 +0100

    hallo

--- View/Login_erfassen.php
+++ View/Login_erfassen.php
@@ -7,6 +7,23 @@
         <div id="container">
         <header>
             <h1>Online Verwaltungstool</h1> 
+            <script type="text/javascript">
+            
+            window.onload = function() {
+                document.getElementById("password").onchange = validatePassword;
+                document.getElementById("confirm_password").onchange = validatePassword;
+            }
+            function validatePassword() {
+                var pass2 = document.getElementById("confirm_password").value;
+                var pass1 = document.getElementById("password").value;
+                if(pass1!==pass2) {
+                    document.getElementById("confirm_password").setCustomValidity("Passwörter sind nicht identisch");
+                } else {
+                    document.getElementById("confirm_password").setCustomValidity('');
+                }
+            }
+            </script>
+            
         </header>
         <!-- Linke Spalte -->
         <nav>        
@@ -20,24 +37,24 @@
         <!-- Mittlere Spalte ( Hauptinhalt -->
             <section id="content">
                 <h2> Neuen Benutzer anlegen</h2>  
-                <form name="formular" onsubmit="return mySubmit()" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">  
+                <form name="newUser" onsubmit="return mySubmit()" action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">  
                     <input type="hidden" name="neuerBenutzer" value="erfassung"/>    
                 <table>
                     <tr>
                     <td>Name:</td>
-                    <td><input type="text" name="lastname" value="" size="40" /></td>
+                    <td><input type="text" name="lastname" value="" size="40" required=""/></td>
                     </tr>
                     <tr>
                     <td>Vorname:</td>
-                    <td><input type="text" name="firstname" value="" size="40" /></td>
+                    <td><input type="text" name="firstname" value="" size="40" required=""/></td>
                     </tr>
                     <tr>
                     <td>Passwort:</td>
-                    <td><input type="text" name="password1" value="" size="40" /></td>
+                    <td><input type="password" pattern=".{6,}" oninvalid="this.setCustomValidity('Passwort benötigt mindestens 6 Zeichen')" id="password" required name="password1" value="" size="40" /></td>
                     </tr>
                     <tr>
                     <td>Passwort wiederholen:</td>
-                    <td><input type="text" name="password2" value="" size="40" /></td>
+                    <td><input type="password" name="password2" value="" size="40" required/></td>
                     </tr>
                     <tr>
                     <td>E-Mail Adresse:</td>
