b61a395cb41cd3e43bb782406e03ed6962739440
Marco <Marco@MarcoTab.edu.ds.fhnw.ch>
2016-11-09 10:09:12 +0100

    login_erfassen validation html5

--- View/Login_erfassen.php
+++ View/Login_erfassen.php
@@ -10,16 +10,16 @@
             <script type="text/javascript">
             
             window.onload = function() {
-                document.getElementById("password").onchange = validatePassword;
-                document.getElementById("confirm_password").onchange = validatePassword;
+                document.getElementById("password1").onchange = validatePassword;
+                document.getElementById("password2").onchange = validatePassword;
             }
             function validatePassword() {
-                var pass2 = document.getElementById("confirm_password").value;
-                var pass1 = document.getElementById("password").value;
+                var pass2 = document.getElementById("password1").value;
+                var pass1 = document.getElementById("password2").value;
                 if(pass1!==pass2) {
-                    document.getElementById("confirm_password").setCustomValidity("Passwörter sind nicht identisch");
+                    document.getElementById("password2").setCustomValidity("Passwörter sind nicht identisch");
                 } else {
-                    document.getElementById("confirm_password").setCustomValidity('');
+                    document.getElementById("password2").setCustomValidity('');
                 }
             }
             </script>
@@ -42,23 +42,23 @@
                 <table>
                     <tr>
                     <td>Name:</td>
-                    <td><input type="text" name="lastname" value="" size="40" required=""/></td>
+                    <td><input type="text" name="lastname" value="" size="40" required oninvalid="this.setCustomValidity('Geben Sie bitte Ihren Namen ein!')" oninput="setCustomValidity('')"/></td>
                     </tr>
                     <tr>
                     <td>Vorname:</td>
-                    <td><input type="text" name="firstname" value="" size="40" required=""/></td>
+                    <td><input type="text" name="firstname" value="" size="40" required oninvalid="this.setCustomValidity('Geben Sie bitte Ihren Vornamen ein!')" oninput="setCustomValidity('')"/></td>
                     </tr>
                     <tr>
                     <td>Passwort:</td>
-                    <td><input type="password" pattern=".{6,}" oninvalid="this.setCustomValidity('Passwort benötigt mindestens 6 Zeichen')" id="password" required name="password1" value="" size="40" /></td>
+                    <td><input type="password" pattern=".{6,}" required oninvalid="this.setCustomValidity('Passwort benötigt mindestens 6 Zeichen')" id="password1" name="password1" value="" size="40" oninput="setCustomValidity('')"/></td>
                     </tr>
                     <tr>
                     <td>Passwort wiederholen:</td>
-                    <td><input type="password" name="password2" value="" size="40" required/></td>
+                    <td><input type="password" name="password2" id="password2" value="" size="40" required oninvalid="this.setCustomValidity('Wiederholen Sie bitte das Passwort!')" oninput="setCustomValidity('')"/></td>
                     </tr>
                     <tr>
                     <td>E-Mail Adresse:</td>
-                    <td><input type="text" name="email" value="" size="40" /></td>
+                    <td><input type="email" name="email" value="" size="40" required oninvalid="this.setCustomValidity('Geben Sie bitte Ihre E-Mail Adresse ein!')" oninput="setCustomValidity('')" placeholder="me@example.com"/></td>
                     </tr>
                     <tr>
                         <td text align = "left"><a href="index.php">Login</a></td>
