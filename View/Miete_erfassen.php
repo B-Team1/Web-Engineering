20b9ea882dc6f5ec3095055008330b9731a132d4
Marco <Marco@MarcoTab.edu.ds.fhnw.ch>
2016-11-09 10:33:25 +0100

    Miete_erfassen validation html5

--- View/Miete_erfassen.php
+++ View/Miete_erfassen.php
@@ -26,17 +26,17 @@
         <!-- Mittlere Spalte ( Hauptinhalt -->
             <section id="content">
                 <h2>Miete erfassen</h2>
-                <form action="Miete_erfassen.php" method="POST">
+                <form name="newRent" action="Miete_erfassen.php" method="POST">
                     <label> Rechnungsdatum:</label>     
-                    <input type="text" name="rechnungsdatum" value="" size="40" /><br/>
+                    <input type="date" name="rechnungsdatum" value="" size="40" required oninvalid="this.setCustomValidity('Wählen Sie bitte das Rechnungsdatum aus!')" oninput="setCustomValidity('')"/><br/>
                     <label> Wohnung:</label>
-                    <input type="text" name="wohnung" value="" size="40" /><br/>
+                    <input type="text" name="wohnung" value="" size="40" required/><br/>
                     <label> Grund:</label>
-                    <input type="text" name="grund" value="Miete" size="40" disabled /><br/>
+                    <input type="text" name="grund" value="Miete" size="40" disabled/><br/>
                     <label> Betrag:</label>
-                    <input type="text" name="betrag" value="" size="40" /><br/>
+                    <input type="text" name="betrag" value="" size="40" required oninvalid="this.setCustomValidity('Geben Sie bitte den Betrag ein!')" oninput="setCustomValidity('')"/><br/>
                     <label> Zahlbar bis:</label>
-                    <input type="text" name="zahlbar_bis" value="" size="40" /><br/> 
+                    <input type="date" name="zahlbar_bis" value="" size="40" required oninvalid="this.setCustomValidity('Wählen Sie bitte das Enddatum aus!')" oninput="setCustomValidity('')"/><br/>
                     <label> Status:</label>
                         <select name="status" style="width:313">
                             <option class="yellow" value="offen">offen</option>
