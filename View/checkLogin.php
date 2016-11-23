b947a00e231ee0b4f676264040ac663a9b46e05e
Tobias <Tobias@Tobias.adm.ds.fhnw.ch>
2016-11-23 14:41:56 +0100

    check login + session

--- View/checkLogin.php
+++ View/checkLogin.php
@@ -1,10 +1,16 @@
 <?php
+
 include_once '../Controller/HirerController.php';
 
-if (isset($_POST['email']) AND isset($_POST['password']))
-{
+session_start();
+
+if (isset($_POST['email']) AND isset($_POST['password'])) {
     $hirerController = new HirerController();
-    if ($hirerController->checkLogin($_POST['email'], $_POST['password'])){
-        echo "<br/><a href=\"Wohnungen.php\"> Hier gehts zu den geheimen Daten</a>";
+    if ($hirerController->checkLogin($_POST['email'], $_POST['password'])) {
+        header("Location: Wohnungen.php");
+        exit();
+    }else{
+        $_SESSION['loginErr'] = True;
+        header("Location: index.php");
     }
 }
