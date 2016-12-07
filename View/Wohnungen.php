20da7334757613c057129b9b78b1045ecf3ed06e
Luzian <Luzian@10.207.18.207>
2016-12-07 10:54:13 +0100

    Wohnungen Tabelle

--- View/Wohnungen.php
+++ View/Wohnungen.php
@@ -38,78 +38,23 @@ include '../Validator/login_pruefen.inc.php';
                         </thead>
                         <tbody>
                     <?php
-                     $rc = new RoomController();
-                    echo $rc->selectAllRoomsByHirer()[0][0];
-                    
-                    /*tabelle generieren*/  
-                    foreach ($zeile as $rc->selectAllRoomsByHirer()) {
+                    $rc = new RoomController();
+                    $sql = $rc->selectRoomTable();
+        
+                    for($i=0; $i<count($sql); $i++) {
+                        $b = $sql[$i];
                         echo "<tr>";
-                        echo "<td>" . $zeile['Bezeichnung'] . "</td>";
-                        echo "<td>" . $zeile['Name'] . "</td>";
-                        echo "<td>" . $zeile['Strasse'] . "</td>";
-                        echo "<td>" . $zeile['Fläche'] . "</td>";
-                        echo "<td>" . $zeile['strasse'] . "</td>";
-                        
+                        for($c=1; $c<count($b); $c++){
+                            echo "<td>". $b[$c] . "</td>";
+                            
+                            
+                        }
+                        echo "<td><a href='Wohnung_bearbeiten.php'><img src='img/bearbeiten_icon.png' alt='' style='width:10px; height:auto;'></a></td>";
+                        echo "<td><a href='Wohnungen.php'><img src='img/loeschen_icon.png' alt='' style='width:15px; height:auto;' onClick='delete_room()'></a></td>";
                         echo "</tr>";
-                   }
+                    }
                     ?>
                         </tbody>    
-                    </table>
-                    
-                    
-                    
-                    <table class="table-responsive" width="100%">
-                        <thead>
-                            <tr>
-                                <th>Wohnung</th>
-                                <th>Mieter</th>
-                                <th>Adresse</th>
-                                <th>Fläche m2</th>
-                                <th>Bearbeiten</th>
-                                <th>Löschen</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><a href=Wohnung_Detailansicht.php>Erdgeschoss 1</a></td>
-                                <td>Famillie Müller</td>
-                                <td>Sonnenweg 6a</td>
-                                <td>80</td>
-                                <td> 
-                                    <a href="Wohnung_bearbeiten.php">
-                                        <img src="img/bearbeiten_icon.png" alt="" style="width:10px; height:auto;">
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="Wohnungen.php">
-                                        <img src="img/loeschen_icon.png" alt="" style="width:15px; height:auto;" onClick="delete_room()">
-                                    </a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><a href=Wohnung_Detailansicht.php>Erdgeschoss 1</a></td>
-                                <td>Famillie Müller</td>
-                                <td>Sonnenweg 6a</td>
-                                <td>80</td>
-                                <td> 
-                                    <a href="Wohnung_bearbeiten.php">
-                                        <img src="img/bearbeiten_icon.png" alt="" style="width:10px; height:auto;">
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href="Wohnungen.php">
-                                        <img src="img/loeschen_icon.png" alt="" style="width:15px; height:auto;" onClick="delete_room(2)">
-                                    </a>
-                                </td>
-                            </tr>
-                            <!-- letzte Zeile für Add-Button -->
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td><a href="Wohnung_erfassen.php" class="myButton">+</a></td>
-                        </tbody> 
                     </table>
                 </div>
             </div>
