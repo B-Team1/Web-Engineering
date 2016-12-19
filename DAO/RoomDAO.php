3d50f494bbb2a21b15f593cb9a80cac1bb791e26
Marco <Marco@MarcoTab.edu.ds.fhnw.ch>
2016-12-19 09:48:17 +0100

    db

--- DAO/RoomDAO.php
+++ DAO/RoomDAO.php
@@ -15,7 +15,7 @@ class RoomDAO extends AbstractDAO{
      * @param Room $room
      */
     public function insertRoom(Room $room) {
-        $insert = "INSERT INTO `mydb`.`wohnung` (`Bezeichnung`, `Fläche`, `Stockwerk`, `Mietzins`, `Vermieter_idVermieter`) "
+        $insert = "INSERT INTO `ateam`.`wohnung` (`Bezeichnung`, `Fläche`, `Stockwerk`, `Mietzins`, `Vermieter_idVermieter`) "
                 . "VALUES ('" . $room->getDescription() . "', '" . $room->getArea() . "', '" . $room->getFloor() . "',"
                 . " '" . $room->getRent() . "', '" . $room->getHirerId() . "');";
         mysqli_query($this->link, $insert) or die(mysqli_error($this->link));
@@ -26,7 +26,7 @@ class RoomDAO extends AbstractDAO{
      * @param Room $room
      */
     public function updateRoom(Room $room) {
-        $sql = "UPDATE `mydb`.`wohnung` SET `Bezeichnung`='" . $room->getDescription() . "', `Fläche`='" . $room->getArea() . "', "
+        $sql = "UPDATE `ateam`.`wohnung` SET `Bezeichnung`='" . $room->getDescription() . "', `Fläche`='" . $room->getArea() . "', "
                 . "`Stockwerk`='" . $room->getFloor() . "', `Mietzins`='" . $room->getRent() . "', "
                 . "`Vermieter_idVermieter`='" . $room->getHirerId() . "' WHERE `idWohnung`='" . $room->getRoomId() . "';";
         mysqli_query($this->link, $sql) or die(mysqli_error($this->link));
@@ -38,7 +38,7 @@ class RoomDAO extends AbstractDAO{
      * @return Array
      */
     public function selectRoomById($id){
-        $sql = "SELECT * FROM mydb.wohnung WHERE mydb.wohnung.idWohnung = ".$id.";";
+        $sql = "SELECT * FROM ateam.wohnung WHERE ateam.wohnung.idWohnung = ".$id.";";
         $result = mysqli_query($this->link, $sql) or die(mysqli_error($this->link));
         if ($result->num_rows > 0) {
             // output data of each row
@@ -56,7 +56,7 @@ class RoomDAO extends AbstractDAO{
      * @param type $id
      */
     public function deleteRoomById($id){
-        $sql = "DELETE FROM `mydb`.`wohnung` WHERE `idWohnung` = ".$id.";";
+        $sql = "DELETE FROM `ateam`.`wohnung` WHERE `idWohnung` = ".$id.";";
         $result = mysqli_query($this->link, $sql) or die(mysqli_error($this->link));
     }
     
@@ -66,8 +66,8 @@ class RoomDAO extends AbstractDAO{
     public function selectRoomTable(){
         $c = 0;
         $arr = array();
-        $sql = "SELECT idWohnung, Bezeichnung, Name, Strasse, Fläche FROM mydb.wohnung 
-                left join mydb.mieter on mydb.mieter.Wohnung_idWohnung = mydb.wohnung.idWohnung";
+        $sql = "SELECT idWohnung, Bezeichnung, Name, Strasse, Fläche FROM ateam.wohnung 
+                left join ateam.mieter on ateam.mieter.Wohnung_idWohnung = ateam.wohnung.idWohnung";
         $result = mysqli_query($this->link, $sql) or die(mysqli_error($this->link));
         if ($result->num_rows > 0) {
             // output data of each row
@@ -82,7 +82,7 @@ class RoomDAO extends AbstractDAO{
     }
     
     public function selectRoomsByHirer($id){
-        $sql = "SELECT * FROM mydb.wohnung inner join mydb.mieter on mydb.wohnung.idWohnung = mydb.mieter.Wohnung_idWohnung WHERE mydb.wohnung.Vermieter_idVermieter =  ".$id.";";
+        $sql = "SELECT * FROM ateam.wohnung inner join ateam.mieter on ateam.wohnung.idWohnung = ateam.mieter.Wohnung_idWohnung WHERE ateam.wohnung.Vermieter_idVermieter =  ".$id.";";
         $result = mysqli_query($this->link, $sql) or die(mysqli_error($this->link));
         $arr = array();
         $c = 0;
@@ -102,8 +102,8 @@ class RoomDAO extends AbstractDAO{
     public function selectFreeRooms($hirerID){
         $c = 0;
         $arr = array();
-        $sql = "SELECT * FROM mydb.wohnung left join mydb.mieter on mydb.wohnung.idWohnung = mydb.mieter.Wohnung_idWohnung"
-                . " WHERE mydb.wohnung.Vermieter_idVermieter = ".$hirerID." and mydb.mieter.Wohnung_idWohnung is null;";
+        $sql = "SELECT * FROM ateam.wohnung left join ateam.mieter on ateam.wohnung.idWohnung = ateam.mieter.Wohnung_idWohnung"
+                . " WHERE ateam.wohnung.Vermieter_idVermieter = ".$hirerID." and ateam.mieter.Wohnung_idWohnung is null;";
         echo $sql;
         $result = mysqli_query($this->link, $sql) or die(mysqli_error($this->link));
         if ($result->num_rows > 0) {
